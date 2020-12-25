af44c2585d07d080990bfcdf2df33f7158b42d22
Rad7950 <adanabbas7950@gmail.com>
2020-12-25 20:00:06 +0500

    Organized Controller.py

--- Backend/OnlineStore/backend/BusinessLogic/Controller.py
+++ Backend/OnlineStore/backend/BusinessLogic/Controller.py
@@ -11,6 +11,8 @@ class Store:
         self.products = products()
         self.complaints = complaints()
 
+    # accounts.py functions
+
     def login_user(self):
         return self.accounts.login_user()
 
@@ -27,15 +29,27 @@ class Store:
             "accountType": account.user_type,
         }
 
+    # user.py functions
+
     def addBillingAddress(self, account_id, address):
         account = self.accounts.getAccount(account_id)
         User = user(account)
         User.addBillingAddress(address)
 
+    def addProductToCart(self, account_id, product_id, quantity):
+        account = self.accounts.getAccount(account_id)
+        product = self.products.getProduct(product_id)
+        User = user(account)
+        User.addProductToCart(product, quantity)
+
+    # vendor.py functions
+
     def editShop(self, account_id, shop_name, shop_location):
         account = self.accounts.getAccount(account_id)
         Vendor = vendor(account)
         Vendor.editShop(shop_name, shop_location)
+
+    # products.py functions
 
     def addProduct(self, account_id, product_name, product_desc, quantity, category, price):
         account = self.accounts.getAccount(account_id)
@@ -54,14 +68,6 @@ class Store:
     def updateProduct(self, product_id, product_name, product_desc, quantity, category, price):
         self.products.updateProduct(product_id, product_name,
                                     product_desc, quantity, category, price)
-
-    def addProductToCart(self, account_id, product_id, quantity):
-        account = self.accounts.getAccount(account_id)
-        product = self.products.getProduct(product_id)
-        User = user(account)
-        User.addProductToCart(product, quantity)
-
-    # adan
 
     def addReview(self, account_id, product_id, stars, feedback):
         account = self.accounts.getAccount(account_id)
